b8b7f14be1850d60f2bf583c24ad96d22967ba44
Rajavi Shah <shahraja@broadinstitute.org>
2025-07-30 15:17:49 -0400

    Fix Vite base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react        from '@vitejs/plugin-react'
 import path         from 'path'
 
 export default defineConfig({
+  base: '/bilayers-config-generator/',
   plugins: [react()],
   resolve: {
     alias: {
